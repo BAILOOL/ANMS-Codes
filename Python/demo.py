e1ac82f9928b962ccf69c5d2debbd703c44530c5
Alex Bailo <alexandr.baylo@gmail.com>
2021-08-04 17:19:11 +0200

    feature/sorting-keypoints-in-python-demo (#32)
    * Updating function class to new opencv api
    
    * Sorting keypoints in python demo
    
    * Fixing linters for python and markdown

--- Python/demo.py
+++ Python/demo.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import argparse
-from random import shuffle
 
 import cv2  # pylint: disable=import-error
 from ssc import ssc
@@ -18,20 +17,21 @@ def main():
     cv2.imshow("Input Image", img)
     cv2.waitKey(0)
 
-    fast = cv2.FastFeatureDetector()
+    fast = cv2.FastFeatureDetector_create()
     keypoints = fast.detect(img, None)
-    img2 = cv2.drawKeypoints(img, keypoints, color=(255, 0, 0))
+    img2 = cv2.drawKeypoints(img, keypoints, outImage=None, color=(255, 0, 0))
     cv2.imshow("Detected FAST keypoints", img2)
     cv2.waitKey(0)
 
-    # keypoints should be sorted by strength in descending order before feeding to SSC to work correctly
-    shuffle(keypoints)  # simulating sorting by score with random shuffle
+    # keypoints should be sorted by strength in descending order
+    # before feeding to SSC to work correctly
+    keypoints = sorted(keypoints, key=lambda x: x.response, reverse=True)
 
     selected_keypoints = ssc(
         keypoints, args.num_ret_points, args.tolerance, img.shape[1], img.shape[0]
     )
 
-    img3 = cv2.drawKeypoints(img, selected_keypoints, color=(255, 0, 0))
+    img3 = cv2.drawKeypoints(img, selected_keypoints, outImage=None, color=(255, 0, 0))
     cv2.imshow("Selected keypoints", img3)
     cv2.waitKey(0)
 
